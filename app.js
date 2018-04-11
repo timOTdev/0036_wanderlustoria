b070e0b0629ef822a24025c50fe0fd105c75ab97
Timothy <timh1203@gmail.com>
2018-04-10 23:11:54 -0400

    Add bootstrap and page routings

--- app.js
+++ app.js
@@ -2,28 +2,36 @@ const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
 
+// Setup
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}))
 
+// INDEX ROUTE
 app.get('/', function(req, res){
     res.render("intro");
 })
 
+app.get('/home', function(req,res){
+    res.render("home");
+})
+
+// NEW ROUTE
 app.get('/destinations', function(req, res){
     res.render("destinations");
 })
 
+app.get('/destinations/new', function(req, res){
+    res.render("addDestination");
+})
+
+// CREATE ROUTE
 app.post('/destinations', function(req, res){
     console.log("You've just added " + req.body + "!");
     res.redirect("/destinations");
 })
 
-app.get('/destinations/add', function(req, res){
-    res.render("addDestination");
-})
-
+// Server Listener
 app.listen(3000, function(){
     console.log("Wanderlustoria is running!");
 })
