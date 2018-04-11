b513941a386ce53cf946d266d2a7750c42aa878a
Timothy <timh1203@gmail.com>
2018-04-10 22:42:45 -0400

    Add body-parser and add route

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
-    res.send("Hello world");
+    res.render("intro");
+})
+
+app.get('/destinations', function(req, res){
+    res.render("destinations");
+})
+
+app.post('/destinations', function(req, res){
+    console.log("You've just added " + req.body + "!");
+    res.redirect("/destinations");
+})
+
+app.get('/destinations/add', function(req, res){
+    res.render("addDestination");
 })
 
 app.listen(3000, function(){
