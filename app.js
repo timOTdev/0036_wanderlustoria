d077a2a3681d58c1ea706ad4bbb00e9bbfad38c5
Timothy Hoang <timh1203@gmail.com>
2018-04-14 14:23:25 -0400

    Add mongo and mongoose

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // Setup
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 // app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost/wanderlustoria");
 
 // Routes
 const homeRoutes = require("./routes/homeRoute");
