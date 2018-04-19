d3dc92abaacf9be416e2a57b5137f09d374c7abd
Timothy Hoang <timh1203@gmail.com>
2018-04-18 23:04:52 -0400

    Add cities show page

--- routes/wanderlustoriaRoute.js
+++ routes/wanderlustoriaRoute.js
@@ -2,12 +2,6 @@ const express = require("express");
 const router = express.Router();
 const City = require("../models/citySchema");
 
-// let cities = [
-//     {name: "Medellin", country: "Colombia", image: "https://images.unsplash.com/photo-1512250431446-d0b4b57b27ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a44ca66de56c79c3f78639b5e948d17a&auto=format&fit=crop&w=1355&q=80"},
-//     {name: "Chiang Mai", country: "Thailand", image: "https://images.unsplash.com/photo-1512553353614-82a7370096dc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=643c6ebcaf321c2a939e57ed8c0eb485&auto=format&fit=crop&w=1489&q=80"},
-//     {name: "Hoi An", country: "Vietnam", image: "https://images.unsplash.com/36/hGibbjg0Rb2fUIoMtU5l__DSC8099.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89cdd0f896a0d49a26d8627cca6b41e3&auto=format&fit=crop&w=1418&q=80"}
-// ];
-
 // INDEX ROUTE
 router.get('/', function(req, res){
     City.find({}, function(err, allCities){
@@ -39,6 +33,18 @@ router.post('/', function(req, res){
 // NEW ROUTE
 router.get('/new', function(req, res){
     res.render("wanderlustoriaNew");
+});
+
+// SHOW ROUTE
+router.get('/:id', function(req, res){
+    City.findById(req.params.id, function(err, foundCity){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCity);
+            res.render("wanderlustoriaShow", {city: foundCity});
+        }
+    });
 });
 
 module.exports = router;
